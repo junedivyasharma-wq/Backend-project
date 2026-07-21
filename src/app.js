96ea2c87172cd41757ea4a422a2b486b20e23338
junedivyasharma-wq <your-github-email@gmail.com>
2026-07-22 02:03:45 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -15,10 +15,14 @@ app.use(cors({
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended: true, limit:"16kb"}))
 app.use(express.static("public"))
-app.use(express.cookieParser())
+app.use(cookieParser())
+
+//routes import
+import userRouter from './routes/user.routes.js'
 
 
 
+// routes declaration
+app.use("/api/v1 /users",userRouter)
 
-
-export default app
+export {app}
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv"
 // import mongoose from "mongoose";
 // import {DB_NAME} from "./constants";
 import connectDB from "./db/index.js";
+import {app} from './app.js';
 
 dotenv.config({
     path:'./env'
@@ -21,8 +22,7 @@ then(()=>{
 })
 
 /*
-import express from "express"
-const app= expres()
+
 ( async () => { 
     try{
         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
